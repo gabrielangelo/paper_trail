cd09453ed03f494c7e930e446c448a458621dbee
gabri005@gmail.com <gabri005@gmail.com>
2026-01-12 03:13:48 -0300

    chore: update ecto to 3.13.5 version

--- mix.exs
+++ mix.exs
@@ -28,16 +28,16 @@ defmodule PaperTrail.Mixfile do
     ]
   end
 
-  defp deps do
-    [
-      {:ecto, ">= 3.9.0"},
-      {:ecto_sql, ">= 3.9.0"},
-      {:ex_doc, ">= 0.21.3"},
-      {:postgrex, ">= 0.0.0", only: [:dev, :test]},
-      {:jason, ">= 1.2.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.0", runtime: false, only: [:dev, :test]}
-    ]
-  end
+defp deps do
+  [
+    {:ecto, "~> 3.13.5"},
+    {:ecto_sql, "~> 3.13.4"},  # Updated to match Ecto version
+    {:ex_doc, ">= 0.21.3"},
+    {:postgrex, ">= 0.0.0", only: [:dev, :test]},
+    {:jason, ">= 1.2.0", only: [:dev, :test]},
+    {:dialyxir, "~> 1.0", runtime: false, only: [:dev, :test]}
+  ]
+end
 
   defp description do
     """
